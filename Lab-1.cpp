5923ff8ca1f7192304642e92185cca1049dc0286
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-01-20 22:04:36 +0600

    Redesign For DSA

--- Lab-1.cpp
+++ Lab-1.cpp
@@ -1,20 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool Step1(string a)
+
+string Q0(string a)
 {
         if(a[0]=='_' || (a[0]>=65&&a[0]<=90)||(a[0]>=97 && a[0]<=122))
-            return true;
+            return "Final";
         else
-            return false;
+            return "Trap";
 }
-bool Step2(int l, string a)
+string Final(int l, string a)
 {
     for (int i = 1; i < l; i++)
     {
         if (!((a[i] >= 65 && a[i] <= 90) || (a[i] >= 97 && a[i] <= 120) || a[i] == '0' || a[i] == '1' || a[i] == '2' || a[i] == '3' || a[i] == '4' || a[i] == '5' || a[i] == '6' || a[i] == '7' || a[i] == '8' || a[i] == '9' || (a[i] == '_')))
-            return false;
+            return "Trap";
     }
-    return true;
+    return "Final";
 }
 
 int main()
@@ -23,18 +24,13 @@ int main()
     cin>>a;
     int l= a.size();
 
-     if(!Step1(a))
+     if(Q0(a)=="Trap")
      {
-     cout<<"Invalid";
+     cout<<Q0(a);
      return 0;
     }
     else
     {
-        if(!Step2(l,a))
-        cout<<"Invalid";
-    else
-        
-        cout<<"Valid";
-    return 0;  
+        cout<<Final(l,a);
     }
 }
