4f05e256d5ff6b3e043bed437bf23250df1d9875
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-11 02:41:41 +0600

    removed const

--- Lab-4.cpp
+++ Lab-4.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-bool isIdentifier(const string& word) {
+bool isIdentifier(string word) {
     for (char c : word) {
         if (!((c >= 'a' && c <= 'z') ||
               (c >= 'A' && c <= 'Z') ||
@@ -17,25 +17,25 @@ bool isIdentifier(const string& word) {
     return true;
 }
 
-bool isSymbol(const string& word) {
+bool isSymbol(string word) {
     return word.size() == 1 && (word[0] == '{' || word[0] == '}' || word[0] == '(' || word[0] == ')' || word[0] == '[' || word[0] == ']' ||
            word[0] == ';' || word[0] == ',' || word[0] == '.' || word[0] == ':' || word[0] == '"' || word[0] == '\'');
 }
 
-bool isCodeOperator(const string& word) {
+bool isCodeOperator(string word) {
     return word.size() == 1 && (word[0] == '=' || word[0] == '+' || word[0] == '-' || word[0] == '*' || word[0] == '/' ||
            word[0] == '%' || word[0] == '<' || word[0] == '>' || word[0] == '!' || word[0] == '&' ||
            word[0] == '|' || word[0] == '^' || word[0] == '~' || word[0] == '?' || word[0] == ':');
 }
 
-bool isKeyword(const string& word) {
+bool isKeyword(string word) {
     return word == "break" || word == "char" || word == "continue" || word == "do" ||
            word == "double" || word == "else" || word == "float" || word == "for" ||
            word == "if" || word == "int" || word == "long" || word == "return" ||
            word == "while" || word == "cout" || word == "cin";
 }
 
-void analyzeCode(const string& code,
+void analyzeCode(string code,
                  vector<string>& keywords,
                  vector<string>& identifiers,
                  vector<string>& symbols,
@@ -66,25 +66,25 @@ int main() {
     analyzeCode(code, keywords, identifiers, symbols, operators);
 
     cout << "Keywords: ";
-    for (const string& word : keywords) {
+    for (string word : keywords) {
         cout << word << " ";
     }
     cout << endl;
 
     cout << "Identifiers: ";
-    for (const string& word : identifiers) {
+    for (string word : identifiers) {
         cout << word << " ";
     }
     cout << endl;
 
     cout << "Symbols: ";
-    for (const string& word : symbols) {
+    for (string word : symbols) {
         cout << word << " ";
     }
     cout << endl;
 
     cout << "Operators: ";
-    for (const string& word : operators) {
+    for (string word : operators) {
         cout << word << " ";
     }
     cout << endl;
